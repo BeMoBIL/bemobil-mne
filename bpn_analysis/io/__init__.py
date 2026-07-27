7657f619aca185d7528eca0c61c848b67e841cfa
Roy Eric <139973278+Randomidous@users.noreply.github.com>
2026-07-27 12:24:49 +0200

    ENH: update I/O func; improve stream alignment and resampling (#3)
    * ADD: neuro-urban walk analysis init
    
    * ENH: improve stream alignment
    
    * update preproc

--- bpn_analysis/io/__init__.py
+++ bpn_analysis/io/__init__.py
@@ -1,5 +1,6 @@
 """I/O utilities for BPN-analysis."""
 
-from bpn_analysis.io.xdf import XDFLoader
+from bpn_analysis.io.alignment import align_stream_to_timestamps
+from bpn_analysis.io.xdf import MultimodalRecording, XDFLoader
 
-__all__ = ["XDFLoader"]
+__all__ = ["XDFLoader", "MultimodalRecording", "align_stream_to_timestamps"]
