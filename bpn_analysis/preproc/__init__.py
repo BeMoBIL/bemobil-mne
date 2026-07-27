b71a382f5f3ec61830c78435368db427a7574156
Roy Eric <139973278+Randomidous@users.noreply.github.com>
2026-07-27 12:29:04 +0200

    FEAT: extend EEGPreprocessor (#4)

--- bpn_analysis/preproc/__init__.py
+++ bpn_analysis/preproc/__init__.py
@@ -1,25 +1,55 @@
 """Preprocessing utilities for BPN-analysis."""
 
 from bpn_analysis.preproc.epoching import EpochPreparer, get_stimulus_rename_map
-from bpn_analysis.preproc.preprocessing import EEGPreprocessor, NumpyEncoder
+from bpn_analysis.preproc.preprocessing import EEGPreprocessor, NumpyEncoder, get_bad_chs
 from bpn_analysis.preproc.utils import (
+    FRONTAL_CHS_EQUIDISTANT,
+    StepTimer,
     _annotate_break_iter,
+    _handle_trans,
+    append_desc,
+    auto_coreg_fsaverage,
+    build_sys_info,
     compute_asr,
     compute_dipolarity,
     compute_ica,
     compute_mi_reduction,
     fit_dipoles_on_ica,
+    format_duration,
+    get_descriptor,
+    get_raw_subset,
+    init_descriptor,
+    set_descriptor,
+    sig_params,
 )
 
 __all__ = [
+    # Classes
     "EEGPreprocessor",
     "EpochPreparer",
     "NumpyEncoder",
-    "get_stimulus_rename_map",
-    "_annotate_break_iter",
+    "StepTimer",
+    # Preprocessing functions
+    "get_bad_chs",
     "compute_asr",
-    "compute_dipolarity",
     "compute_ica",
-    "compute_mi_reduction",
     "fit_dipoles_on_ica",
+    "compute_dipolarity",
+    "compute_mi_reduction",
+    "get_raw_subset",
+    "auto_coreg_fsaverage",
+    # Provenance
+    "init_descriptor",
+    "get_descriptor",
+    "set_descriptor",
+    "append_desc",
+    "sig_params",
+    # Utilities
+    "format_duration",
+    "build_sys_info",
+    "_annotate_break_iter",
+    "_handle_trans",
+    "FRONTAL_CHS_EQUIDISTANT",
+    # Epoching
+    "get_stimulus_rename_map",
 ]
